97bfd2e76906c417d3de9ebe9f6f34797d6c194b
JoylineMenezes <joylinthomson@gmail.com>
2021-05-30 21:47:05 +0530

    timer for all 3 one words are set

--- speedCheck.js
+++ speedCheck.js
@@ -214,30 +214,30 @@ resetButton1.addEventListener("click", reset1, false);
 //------------------------------------------------------------------------------------------------------------------
 
 // //password timer
-// //taking value from the signup_form
-// var form0 = document.querySelector(".signup_form");
-// var submit0 = document.querySelector("#submit");
-// var email;
-// var password0;
-// var conpassword;
-//
-// form0.addEventListener('submit', function(event){
-//   event.preventDefault(); //prevents the form from auto submitting
-//   email = document.getElementById("email").value;
-//   password0 = document.getElementById("password0").value;
-//   conpassword = document.getElementById("confirm_password").value;
-//
-//   if(password0 != conpassword){
-//     alert("Passwords do not match");
-//     // password0.style.borderColor = "red";
-//     // conpassword.style.borderColor = "red";
-//     // password0.value = "";
-//     // conpassword.value = "";
-//   }
-//   console.log(email);
-//   console.log(password0);
-//
-// });
+//taking value from the signup_form
+var form0 = document.querySelector(".signup_form");
+var submit0 = document.querySelector("#submit");
+var email;
+var password0;
+var conpassword;
+
+form0.addEventListener('submit', function(event){
+  event.preventDefault(); //prevents the form from auto submitting
+  email = document.getElementById("email").value;
+  password0 = document.getElementById("password0").value;
+  conpassword = document.getElementById("confirm_password").value;
+
+  if(password0 != conpassword){
+    alert("Passwords do not match");
+    // password0.style.borderColor = "red";
+    // conpassword.style.borderColor = "red";
+    // password0.value = "";
+    // conpassword.value = "";
+  }
+  console.log(email);
+  console.log(password0);
+
+});
 //
 // //timer function
 // //pass1 - comparing and timing the re-type password 1
@@ -306,15 +306,210 @@ resetButton1.addEventListener("click", reset1, false);
 // resetTimer11.addEventListener('click', reset11, false);
 // restFormButton.addEventListener('click', reset11, false);
 
+// -------------------------------------------------------------------------------------------
 
+//timer for one word text box
 
+//                   -WORD TEXT BOX  1-
+const testWord1 = document.querySelector("#text_word1");
+const originalWord1 = "environment";
+const wordResetButton1 = document.querySelector("#reset4");
+const wordTheTimer1 = document.querySelector(".timer4");
 
+var w_timer1 = [0, 0, 0, 0];
+var w_interval1;
+var w_timerRunning1 = false;
 
+// Run a standard minute/second/hundredths timer:
+function w_runTimer1() {
+    let w_currentTime1 = leadingZero(w_timer1[0]) + ":" + leadingZero(w_timer1[1]) + ":" + leadingZero(w_timer1[2]);
+    wordTheTimer1.innerHTML = w_currentTime1;
+    w_timer1[3]++;
 
+    w_timer1[0] = Math.floor((w_timer1[3]/100)/60);
+    w_timer1[1] = Math.floor((w_timer1[3]/100) - (w_timer1[0] * 60));
+    w_timer1[2] = Math.floor(w_timer1[3] - (w_timer1[1] * 100) - (w_timer1[0] * 6000));
+}
 
+// Match the text entered with the provided text on the page:
+function w_spellCheck1() {
+    let w_textEntered1 = testWord1.value ;
+    let w_originTextMatch1 = originalWord1.substring(0,w_textEntered1.length);
+    console.log(w_textEntered1);
+    console.log("hello");
+    if (w_textEntered1 == originalWord1) {
+        clearInterval(w_interval1);
+        // testWrapper.style.borderColor = "#429890";
+    // } else {
+    //     if (textEntered == originTextMatch) {
+    //         testWrapper.style.borderColor = "#65CCf3";
+    //     } else {
+    //         testWrapper.style.borderColor = "#E95D0F";
+    //     }
+    // }
+}
+}
 
+// Start the timer:
+function w_start1() {
+    let w_textEnterdLength1 = testWord1.value.length;
+    if (w_textEnterdLength1 === 0 && !w_timerRunning1) {
+        w_timerRunning1 = true;
+        w_interval1 = setInterval(w_runTimer1, 10);
+    }
+    console.log(w_textEnterdLength1);
+}
 
+// Reset everything:
+function w_reset1() {
+    clearInterval(w_interval1);
+    w_interval1 = null;
+    w_timer1 = [0,0,0,0];
+    w_timerRunning1 = false;
 
+    testWord1.value = "";
+    wordTheTimer1.innerHTML = "00:00:00";
+}
+
+// Event listeners for keyboard input and the reset
+testWord1.addEventListener("keypress", w_start1, false);
+testWord1.addEventListener("keyup", w_spellCheck1, false);
+wordResetButton1.addEventListener("click", w_reset1, false);
+
+//-----------------
+//                   -WORD TEXT BOX  2-
+const testWord2 = document.querySelector("#text_word2");
+const originalWord2 = "misfortunate";
+const wordResetButton2 = document.querySelector("#reset5");
+const wordTheTimer2 = document.querySelector(".timer5");
+
+var w_timer2 = [0, 0, 0, 0];
+var w_interval2;
+var w_timerRunning2 = false;
+
+// Run a standard minute/second/hundredths timer:
+function w_runTimer2() {
+    let w_currentTime2 = leadingZero(w_timer2[0]) + ":" + leadingZero(w_timer2[1]) + ":" + leadingZero(w_timer2[2]);
+    wordTheTimer2.innerHTML = w_currentTime2;
+    w_timer2[3]++;
+
+    w_timer2[0] = Math.floor((w_timer2[3]/100)/60);
+    w_timer2[1] = Math.floor((w_timer2[3]/100) - (w_timer2[0] * 60));
+    w_timer2[2] = Math.floor(w_timer2[3] - (w_timer2[1] * 100) - (w_timer2[0] * 6000));
+}
+
+// Match the text entered with the provided text on the page:
+function w_spellCheck2() {
+    let w_textEntered2 = testWord2.value ;
+    let w_originTextMatch2 = originalWord2.substring(0,w_textEntered2.length);
+    console.log(w_textEntered2);
+    // console.log("hello");
+    if (w_textEntered2 == originalWord2) {
+        clearInterval(w_interval2);
+        // testWrapper.style.borderColor = "#429890";
+    // } else {
+    //     if (textEntered == originTextMatch) {
+    //         testWrapper.style.borderColor = "#65CCf3";
+    //     } else {
+    //         testWrapper.style.borderColor = "#E95D0F";
+    //     }
+    // }
+}
+}
+
+// Start the timer:
+function w_start2() {
+    let w_textEnterdLength2 = testWord2.value.length;
+    if (w_textEnterdLength2 === 0 && !w_timerRunning2) {
+        w_timerRunning2 = true;
+        w_interval2 = setInterval(w_runTimer2, 10);
+    }
+    console.log(w_textEnterdLength2);
+}
+
+// Reset everything:
+function w_reset2() {
+    clearInterval(w_interval2);
+    w_interval2 = null;
+    w_timer2 = [0,0,0,0];
+    w_timerRunning2 = false;
+
+    testWord2.value = "";
+    wordTheTimer2.innerHTML = "00:00:00";
+}
+
+// Event listeners for keyboard input and the reset
+testWord2.addEventListener("keypress", w_start2, false);
+testWord2.addEventListener("keyup", w_spellCheck2, false);
+wordResetButton2.addEventListener("click", w_reset2, false);
+
+//!!!!!!!!!!!!!!!!!!!!!!
+//-----------------
+//                   -WORD TEXT BOX  3-
+const testWord3 = document.querySelector("#text_word3");
+const originalWord3 = "delightful";
+const wordResetButton3 = document.querySelector("#reset6");
+const wordTheTimer3 = document.querySelector(".timer6");
+
+var w_timer3 = [0, 0, 0, 0];
+var w_interval3;
+var w_timerRunning3 = false;
+
+// Run a standard minute/second/hundredths timer:
+function w_runTimer3() {
+    let w_currentTime3 = leadingZero(w_timer3[0]) + ":" + leadingZero(w_timer3[1]) + ":" + leadingZero(w_timer3[2]);
+    wordTheTimer3.innerHTML = w_currentTime3;
+    w_timer3[3]++;
+
+    w_timer3[0] = Math.floor((w_timer3[3]/100)/60);
+    w_timer3[1] = Math.floor((w_timer3[3]/100) - (w_timer3[0] * 60));
+    w_timer3[2] = Math.floor(w_timer3[3] - (w_timer3[1] * 100) - (w_timer3[0] * 6000));
+}
+
+// Match the text entered with the provided text on the page:
+function w_spellCheck3() {
+    let w_textEntered3 = testWord3.value ;
+    let w_originTextMatch3 = originalWord3.substring(0,w_textEntered3.length);
+    console.log(w_textEntered3);
+    // console.log("hello");
+    if (w_textEntered3 == originalWord3) {
+        clearInterval(w_interval3);
+        // testWrapper.style.borderColor = "#429890";
+    // } else {
+    //     if (textEntered == originTextMatch) {
+    //         testWrapper.style.borderColor = "#65CCf3";
+    //     } else {
+    //         testWrapper.style.borderColor = "#E95D0F";
+    //     }
+    // }
+}
+}
+
+// Start the timer:
+function w_start3() {
+    let w_textEnterdLength3 = testWord3.value.length;
+    if (w_textEnterdLength3 === 0 && !w_timerRunning3) {
+        w_timerRunning3 = true;
+        w_interval3 = setInterval(w_runTimer3, 10);
+    }
+    console.log(w_textEnterdLength3);
+}
+
+// Reset everything:
+function w_reset3() {
+    clearInterval(w_interval3);
+    w_interval3 = null;
+    w_timer3 = [0,0,0,0];
+    w_timerRunning3 = false;
+
+    testWord3.value = "";
+    wordTheTimer3.innerHTML = "00:00:00";
+}
+
+// Event listeners for keyboard input and the reset
+testWord3.addEventListener("keypress", w_start3, false);
+testWord3.addEventListener("keyup", w_spellCheck3, false);
+wordResetButton3.addEventListener("click", w_reset3, false);
 
 
 
